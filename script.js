f81792a8ac49f806edf8c178da426ab8ff875ce7
Jleal60 <134532990+Jleal60@users.noreply.github.com>
2024-05-13 13:31:12 +0000

    5/13

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let Sweet = ["https://link-hosting-curriciulum-dev.glitch.me/uploaded/2254c5d6a60c3373096d1e1d9248612f.jpg ", "https://whsat-imagehosting.glitch.me/uploaded/2730edfc300541103fb9733348090cee.jpg "]
-  , Sour = ["https://whsat-imagehosting.glitch.me/uploaded/85d559bc3cceb05efd043c92a6346115.jpg", "https://whsat-imagehosting.glitch.me/uploaded/ee850ff3ec8319e958dd8d14e89720ec.jpg "]
-  , Bitter = ["https://whsat-imagehosting.glitch.me/uploaded/c89d8e2e912042e43c278c7eff24435f.jpg", "https://whsat-imagehosting.glitch.me/uploaded/d0ee69b99156627ee05c4d91d91b7178.jpg"];
+let Sweet = ["pancake.jpg", "doughnut.jpg"]
+  , Sour = ["citrus.jpg", "lime.webp"]
+  , Bitter = ["coffee.jpg", "chocolate.jpg"];
 $(".submit-button").click(function() {
     let e = $(".get-suggestions").val();
     if ($(".breakfast").empty(),
@@ -11,7 +11,7 @@ $(".submit-button").click(function() {
         for (let e of Sour)
             $(".breakfast").append("<img src=" + e + ">");
     else if ("Bitter" === e)
-        for (let e of action)
+        for (let e of Bitter)
             $(".breakfast").append("<img src=" + e + ">")
 }),
 $(".suggestion-button").click(function() {
